c777c4e74afb2c1374a77313e06affcfaf63219c
Le grub <Le grub@DESKTOP-618T81V>
2024-01-25 10:36:29 +0100

    Ajoutez des fichiers projet.

---
+++ LethalHands/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LethalHands
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class LethalHands : BaseUnityPlugin
+    {
+        private const string modGUID = "SlapitNow.LethalHands";
+        private const string modName = "Lethal Hands";
+        private const string modVersion = "22.0.0";
+
+        private static LethalHands instance;
+        private readonly Harmony harmony = new Harmony(modGUID);
+        internal ManualLogSource manualLogSource;
+
+
+
+        void Awake()
+        {
+            if(instance == null) instance = this;
+            manualLogSource = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            manualLogSource.LogInfo("Successfully caught these hands");
+            harmony.PatchAll(typeof(LethalHands));
+
+        }
+    }
+}
