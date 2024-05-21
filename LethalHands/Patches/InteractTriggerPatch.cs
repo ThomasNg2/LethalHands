7d19af69aa8c59ca2112972c631a2b88638f29b6
tnguyen <Thnguyen2802@gmail.com>
2024-05-21 19:05:50 +0200

    Squaring down upon climbing ladders

---
+++ LethalHands/Patches/InteractTriggerPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace LethalHands.Patches
+{
+    [HarmonyPatch(typeof(InteractTrigger))]
+    internal class InteractTriggerPatch
+    {
+        [HarmonyPatch("SetUsingLadderOnLocalClient")]
+        [HarmonyPrefix]
+        static void PreSetUsingLadderOnLocalClient()
+        {
+            LethalHands.Instance.SquareDown(false); 
+        }
+    }
+}
